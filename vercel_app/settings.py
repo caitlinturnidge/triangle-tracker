5c1ccade58ed92fc0c9b620492b6f2a6da9c842e
Caitlin Turnidge <caitlinturnidge@Caitlins-MacBook-Pro.local>
2024-04-10 19:49:13 +0100

    add db to settings

--- vercel_app/settings.py
+++ vercel_app/settings.py
@@ -12,10 +12,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -78,7 +81,19 @@ WSGI_APPLICATION = 'vercel_app.wsgi.app'
 # Note: Django modules for using databases are not support in serverless
 # environments like Vercel. You can use a database over HTTP, hosted elsewhere.
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': environ["DB_NAME"],
+        'USER': environ["DB_USER"],
+        'PASSWORD': environ["DB_PASSWORD"],
+        'HOST': environ["DB_HOST"],
+        'PORT': environ["DB_PORT"],
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
+    }
+}
 
 
 # Password validation
